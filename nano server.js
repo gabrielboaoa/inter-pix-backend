93cb4853d5f96c0b567e876da64028b2e1356712
Gabriel Moraes <obiel.mgm@gmail.com>
2025-06-22 23:10:09 -0300

    Corrigido: arquivos package.json e server.js criados

---
+++ nano server.js
@@ -0,0 +1,15 @@
+{
+  "name": "inter-pix-backend",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.8",
+    "express": "^4.19.2",
+    "fs": "^0.0.1-security",
+    "https": "^1.0.0"
+  }
+}
+
